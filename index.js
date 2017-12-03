c38a93bd56bd4959d629efbfab72c0eb2039bbba
Teemu Kokkinen <kokkinen.teemu@gmail.com>
2017-12-03 17:58:12 +0200

    Added libraries winston for logging and dateformat for formatting timestamps.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 'use strict';
 
-var express = require('express');
+const express = require('express');
+const winston = require('winston');
+const dateformat = require('dateformat');
 var app = express();
 
 var port = 8010;
 
+// Initializes logger.
+const logger = new (winston.Logger)({
+  level: 'info',
+  transports: [
+    new winston.transports.Console({
+    timestamp() {
+      return dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss');
+    },
+    colorize: true
+    })
+  ]
+});
+
+// Initializes routes.
 app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/html/index.html');
 });
 
 app.listen(port, function () {
-  var message = 'Node Express example app listening on port ' + port + '!';
-  console.log(message);
+    logger.info('Node Express example app listening on port ' + port + '!');
 });
